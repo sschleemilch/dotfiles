d9930e564764a924214cc3ef8d6c4146eb78f1e6
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-08-11 11:18:29 +0200

    nvim: snippets and first python snippet

--- dotfiles/config/nvim/lua/plugins/coding.lua
+++ dotfiles/config/nvim/lua/plugins/coding.lua
@@ -93,4 +93,29 @@ return {
       require("cmp").setup(opts)
     end,
   },
+  {
+    "garymjr/nvim-snippets",
+    event = "InsertEnter",
+    config = true,
+    keys = {
+      {
+        "<Tab>",
+        function()
+          return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+      {
+        "<S-Tab>",
+        function()
+          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+    },
+  }
 }
