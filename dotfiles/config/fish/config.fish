11dbc5a45173fee668bdb11216ca24627231ee3e
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-10-20 23:03:24 +0200

    Fish transient prompt

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
+  function starship_transient_prompt_func
+    starship module character
+  end
   starship init fish | source
+  enable_transience
   direnv hook fish | source
 end
 
