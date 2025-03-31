4b9ae2cf1a10b50253f475cb828b43191f257c50
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-03-31 23:37:25 +0200

    nvim: upgraded slimline opts

--- dotfiles/config/nvim/lua/plugins/slimline.lua
+++ dotfiles/config/nvim/lua/plugins/slimline.lua
@@ -3,20 +3,17 @@ return {
     'sschleemilch/slimline.nvim',
     -- 'slimline.nvim',
     lazy = false,
-    dev = false,
+    -- dev = true,
     opts = {
       style = 'fg',
-      bold = true,
-      mode_follow_style = false,
-      verbose_mode = true,
-      workspace_diagnostics = true,
-      sep = {
-        hide = {
-          first = false,
-          last = false,
+      configs = {
+        mode = {
+          style = 'bg',
+          verbose = true,
         },
-        right = '',
-        left = '',
+        diagnostics = {
+          workspace = false,
+        },
       },
     },
   },
