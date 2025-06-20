d9560db4219a47620ebcec382a4bdbb6dfa2bd3f
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-06-20 09:06:40 +0200

    nvim: fidget top + vim.notify

--- dot_config/nvim/lua/plugins/fidget.lua
+++ dot_config/nvim/lua/plugins/fidget.lua
@@ -2,8 +2,12 @@ return {
     'j-hui/fidget.nvim',
     opts = {
         notification = {
+            override_vim_notify = true,
+            view = {
+                stack_upwards = false,
+            },
             window = {
-                winblend = 0,
+                align = 'top',
             },
         },
     },
