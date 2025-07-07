3766183675b126197204ae612c654c8ec1f20212
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-07-07 10:10:12 +0200

    nvim: rose-pine - disabled bold and italic

--- dot_config/nvim/lua/plugins/rose-pine.lua
+++ dot_config/nvim/lua/plugins/rose-pine.lua
@@ -1,6 +1,10 @@
 MiniDeps.now(function()
     MiniDeps.add('rose-pine/neovim')
     require('rose-pine').setup({
+        styles = {
+            bold = false,
+            italic = false,
+        },
         dim_inactive_windows = false,
         highlight_groups = {
             -- General
