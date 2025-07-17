5692808264ad9a4a57b739ade4eb0db1c5a24947
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-07-17 10:25:52 +0200

    nvim: -trouble

--- dot_config/nvim/lua/plugins/trouble.lua
+++
@@ -1,8 +0,0 @@
-MiniDeps.later(function()
-    MiniDeps.add('folke/trouble.nvim')
-    require('trouble').setup({
-        auto_preview = false,
-        auto_close = true,
-    })
-    vim.keymap.set('n', '<leader>tt', '<cmd>Trouble diagnostics toggle<cr>', { desc = 'Diagnostics (trouble)' })
-end)
