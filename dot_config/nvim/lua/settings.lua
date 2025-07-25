045711fbae151e8e70628115ad058cde001b75ad
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-07-25 13:57:09 +0200

    nvim: settings -> diagnostic icons -> circles

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -37,10 +37,10 @@ vim.o.pumheight = 15
 local diagnostic_opts = {
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = ' ',
-            [vim.diagnostic.severity.WARN] = ' ',
-            [vim.diagnostic.severity.HINT] = ' ',
-            [vim.diagnostic.severity.INFO] = ' ',
+            [vim.diagnostic.severity.ERROR] = '●',
+            [vim.diagnostic.severity.WARN] = '●',
+            [vim.diagnostic.severity.HINT] = '●',
+            [vim.diagnostic.severity.INFO] = '●',
         },
     },
     virtual_text = {
