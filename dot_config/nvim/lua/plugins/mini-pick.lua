9f33c7394d6bed271038ce35c673674be2200ba1
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-07-29 08:25:42 +0200

    nvim: mini.pick centered

--- dot_config/nvim/lua/plugins/mini-pick.lua
+++ dot_config/nvim/lua/plugins/mini-pick.lua
@@ -1,6 +1,18 @@
 MiniDeps.later(function()
+    local win_config = function()
+        local height = math.floor(0.618 * vim.o.lines)
+        local width = math.floor(0.618 * vim.o.columns)
+        return {
+            anchor = 'NW',
+            height = height,
+            width = width,
+            row = math.floor(0.5 * (vim.o.lines - height)),
+            col = math.floor(0.5 * (vim.o.columns - width)),
+            border = 'solid',
+        }
+    end
     require('mini.pick').setup({
-        window = { config = { border = 'solid' } },
+        window = { config = win_config },
     })
     vim.ui.select = MiniPick.ui_select
 
