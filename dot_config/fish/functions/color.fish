7214e8e37e21d6c7b02134357fbcf5ff2279acfb
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-08-20 14:08:33 +0200

    fish: +color(), kitty: rose-pine borders

---
+++ dot_config/fish/functions/color.fish
@@ -0,0 +1,15 @@
+function color -d 'change colorscheme'
+    set -l colors "rose-pine" "tokyonight" "kanagawa" "catppuccin" "carbonfox"
+    set -l config_file ~/.config/chezmoi/chezmoi.toml
+    set -l selected_color (printf "%s\n" $colors | fzf)
+
+    if test -n "$selected_color"
+        echo "setting colorscheme '$selected_color'"
+        sed -i '' "s/colorscheme = \".*\"/colorscheme = \"$selected_color\"/" $config_file
+        chezmoi apply
+        kitten @ action load_config_file
+        . ~/.config/fish/config.fish
+    else
+        echo "No color scheme selected"
+    end
+end
