7acf324b0fa82b67a2b66a2c5baeeb1baa195a40
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2025-08-21 21:43:32 +0200

    feat: +color:vague

--- dot_config/fish/functions/color.fish
+++ dot_config/fish/functions/color.fish
@@ -1,5 +1,5 @@
 function color -d 'change colorscheme'
-    set -l colors "rose-pine" "rose-pine-dawn" "rose-pine-moon" "tokyonight-night" "kanagawa" "catppuccin-mocha" "carbonfox" "techbase"
+    set -l colors "rose-pine" "rose-pine-dawn" "rose-pine-moon" "tokyonight-night" "kanagawa" "catppuccin-mocha" "carbonfox" "techbase" "vague"
     set -l config_file ~/.config/chezmoi/chezmoi.toml
     set -l selected_color (printf "%s\n" $colors | fzf)
 
