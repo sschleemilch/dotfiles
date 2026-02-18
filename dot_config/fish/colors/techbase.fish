0fc26eb121e6abb7255b35631352afd1917fdcdd
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2026-02-18 19:42:12 +0100

    colorschemes: -techbase

--- dot_config/fish/colors/techbase.fish
+++
@@ -1,1 +0,0 @@
-set -x FZF_COLORS "--color=gutter:-1,bg+:#121e42,pointer:#1a8c9b,info:#474b65,hl:#1a8c9b,hl+:#1a8c9b,prompt:#1a8c9b"
--- dot_config/nvim/lua/colorschemes/techbase.lua
+++
@@ -1,8 +0,0 @@
-return {
-    src = 'https://github.com/mcauley-penney/techbase.nvim',
-    setup = function()
-        require('techbase').setup({})
-        vim.cmd.colorscheme('techbase')
-        vim.api.nvim_set_hl(0, 'DiagnosticHint', { link = 'DiagnosticInfo' })
-    end,
-}
