b3dcf7bd0d323233a36102a683782b85173fd658
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2026-04-13 12:48:35 +0200

    nvim: lsp_progress_ignore list

--- dot_config/nvim/plugin/12_autocommands.lua
+++ dot_config/nvim/plugin/12_autocommands.lua
@@ -12,7 +12,17 @@ Config.new_autocmd('BufReadPost', nil, function(args)
     end
 end, 'Go to the last location when opening a buffer')
 
+local lsp_progress_ignore = {
+    'ltex_plus',
+}
+
 Config.new_autocmd('LspProgress', nil, function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+    if client and vim.tbl_contains(lsp_progress_ignore, client.name) then
+        return
+    end
+
     local value = ev.data.params.value or {}
     local msg = value.message or 'done'
 
