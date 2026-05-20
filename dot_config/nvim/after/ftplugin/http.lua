e5af49a416b95406cd8b3148dbbe82128cba3066
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2026-05-20 22:32:31 +0200

    nvim: kulala keybinds

--- dot_config/nvim/after/ftplugin/http.lua
+++ dot_config/nvim/after/ftplugin/http.lua
@@ -16,6 +16,14 @@ if ok then
     vim.keymap.set('n', '<localleader>o', function()
         kulala.open()
     end, { desc = 'Kulala open' })
+
+    vim.keymap.set('n', '<localleader>e', function()
+        kulala.set_selected_env()
+    end, { desc = 'Select environment' })
+
+    vim.keymap.set('n', '<localleader>X', function()
+        kulala.clear_cached_files()
+    end, { desc = 'Clear cached files' })
 else
     vim.notify('could not set http kulala keymaps', vim.log.levels.WARN)
 end
