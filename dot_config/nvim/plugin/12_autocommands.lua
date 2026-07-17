776557e7be09b65b03c4f9e9209babf6e42313a1
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2026-07-17 13:01:33 +0200

    nvim: readd last buffer loc autocommand

--- dot_config/nvim/plugin/12_autocommands.lua
+++ dot_config/nvim/plugin/12_autocommands.lua
@@ -2,3 +2,12 @@
 Config.new_autocmd('TextYankPost', nil, function()
     vim.hl.on_yank({ timeout = 150 })
 end, 'Highlight on yank')
+
+-- go to last loc when opening a buffer
+Config.new_autocmd('BufReadPost', nil, function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+        vim.cmd('normal! g`"zz')
+    end
+end, 'Go to the last location when opening a buffer')
