817a2828c369df0b47167926715a0efc4713b245
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2026-08-21 06:31:44 +0200

    hypr: +vague

---
+++ dot_config/hypr/colorscheme/vague.lua
@@ -0,0 +1,58 @@
+local black = 'rgb(141415)'
+local shadow = 'rgb(1c1c24)'
+local graphite = 'rgb(252530)'
+local onyx = 'rgb(333738)'
+local muted = 'rgb(606079)'
+local gray = 'rgb(878787)'
+local white = 'rgb(cdcdcd)'
+local whiteAlpha = '#cdcdcd'
+local yellow = 'rgb(f3be7c)'
+local amber = 'rgb(e8b589)'
+local gold = 'rgb(e0a363)'
+local peach = 'rgb(c48282)'
+local red = 'rgb(d8647e)'
+local storm = 'rgb(405065)'
+local lilac = 'rgb(c3c3d5)'
+local cyan = 'rgb(aeaed1)'
+local magenta = 'rgb(bb9dbd)'
+local aqua = 'rgb(b4d4cf)'
+local lavender = 'rgb(90a0b5)'
+local teal = 'rgb(9bb4bc)'
+local blue = 'rgb(6e94b2)'
+local iris = 'rgb(7e98e8)'
+local green = 'rgb(7fa563)'
+
+-- "black": "#141415",
+-- "shadow": "#1c1c24",
+-- "graphite": "#252530",
+-- "onyx": "#333738",
+-- "muted": "#606079",
+-- "gray": "#878787",
+-- "white": "#cdcdcd",
+-- "yellow": "#f3be7c",
+-- "amber": "#e8b589",
+-- "gold": "#e0a363",
+-- "peach": "#c48282",
+-- "red": "#d8647e",
+-- "storm": "#405065",
+-- "lilac": "#c3c3d5",
+-- "cyan": "#aeaed1",
+-- "magenta": "#bb9dbd",
+-- "aqua": "#b4d4cf",
+-- "lavender": "#90a0b5",
+-- "teal": "#9bb4bc",
+-- "blue": "#6e94b2",
+-- "iris": "#7e98e8",
+-- "green": "#7fa563"
+
+return {
+  accent = teal,
+  background = black,
+  foreground = white,
+  foregroundAlpha = whiteAlpha,
+  surface = graphite,
+  border_active = white,
+  border_inactive = muted,
+  warning = yellow,
+  error = red,
+}
