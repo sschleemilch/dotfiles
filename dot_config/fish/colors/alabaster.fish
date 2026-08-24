1968953f96cf31da30137b1925c684115080f1ac
Sebastian Schleemilch <sebastian.schleemilch@bmw.de>
2026-08-24 10:38:19 +0200

    k9s: +alabaster

--- dot_config/fish/colors/alabaster.fish
+++ dot_config/fish/colors/alabaster.fish
@@ -1,1 +1,1 @@
-set -x FZF_COLORS "--color=bg:#f7f7f7,gutter:#f7f7f7,current-bg:#bfdbfe,current-fg:#000000,hl:#aa3731,hl+:#aa3731,border:#9da39a,pointer:#007acc,info:#9da39a,spinner:#aa3731"
+set -x FZF_COLORS "--color=bg:#f7f7f7,gutter:#f7f7f7,current-bg:#f0f0f0,current-fg:#000000,hl:#448c27,hl+:#448c27,border:#9da39a,pointer:#007acc,info:#9da39a,spinner:#aa3731"
